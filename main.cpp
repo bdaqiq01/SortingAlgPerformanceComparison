22f674780071794113a58792e6ac44c58521afbf
bdaqiq01 <bdaqiq01@gmail.com>
2024-11-26 23:29:27 -0800

    Algorithem updated, readme contents, adding the Makefile

--- main.cpp
+++ main.cpp
@@ -5,8 +5,20 @@
 #include <random>
 #include <numeric>
 #include <chrono>
+#include <time.h>
+#include <limits.h>
+#include<math.h>
+#include<fstream>
 #include "plotSort.h"
+#include <random>  
+#include <fstream>
+#include <iomanip> // 
+#include <sstream>
+#include <filesystem>
 using namespace std;
+
+
+
 
 bool isSorted(const int arr[], int n)
 {
@@ -26,37 +38,6 @@ void printArray(int arr[], int size)
     cout << endl; 
 }
 
-/* bublesort (user api) start with the first item compare to the next item if one is bigger than the other then swap, this puts the biggest number in its place, 
-after each pass the largest item of the unsorted portion bubbles up to its correct position at the end 
-so at the worst case T(n) t(n-1) + n, proof that this is O(n)
-expanding T(n)
-T(n)  = T(n-2) + (n-1) + n 
-    = T(n-3) + (n-2)+ (n-1) + n 
-    = T(1) +2+3 +... +(n-1)+ n // this expands to the sum of n intergers aka 1+2+3+ ....+ n 
-    = T(1) + [ from k = 2 to n of k, T(1) is constant we can simplify 
-    [ from k = 2 to n of k = [ from k = 1 to n of k - 1, cause we are adding one and susbstracting 1 
-    Based on the sum of arithmatic series( addition of series of number) the sum of the first n numbers is equal to 
-    k = 1 to k = n, n(n+1)/2 here as N gets larger the dominent factor is the n^2 not the other ones the variable with the highest power 
-    therefore we say it is O(n)
-    proof of arithmatic ser
-    Sn = 1 + 2 + .... + (n-1) + n 
-    sn = n +(n-1) + ....+ 2 + 1 
-    add the two equation togather 
-    2Sn = (n+1) + (2+(n-1)) + (3+(n-2)) + ...+ (n-1+2) + (n+1) , each pair sums to n+1 and there are n terms 
-    2Sn = n(n+1)  -> Sn = n(n+1)/2
-
-    for the bubble sort we initally everything is unsorted lets say indicated by color black
-    at 0+1 step is the is all is unsorted except the last element lets say indicated by color green. 
-    lets k is keeping track of the sorted rgion since the green region is expanding. 
-    in each pass the last swap that can possibly happen is between k -1 and k -2 
-    So I want iterate to swap between every item up until it is smaller than k, then if no swap is needed
-    substract k by 1 and start from index 0 one more time
-
-    Best case: if the array is already sorted, the alg still will run the same amount of time 
-    so lets pick up if the array is already sorted to improve its performence. if realize in the first pass 
-    mp swaps happend then we can determine that the array is already sorted 
-
-*/
 void bubbleSort(int arr[], int n) //in c++ an array is automatically passed by refeence, therefore the function directly operates on the original array
 {
     int k = n; //k is the black region 
@@ -82,17 +63,6 @@ void bubbleSort(int arr[], int n) //in c++ an array is automatically passed by r
 
 }
 
-/* insertion sort:
-start with assumption that the first element is already sorted, and the remaining elements arenot
-pick the next element in the unsorted part of the list and insert it in the correct location in the sorted part
-by comparing it to every lement in the sorted list, moving from right to left
-shift all the element that are greater to make room for the new element
-at the very begining our region looks like the first element is green the rest are black 
-the middle time looks like the half way are green and the last time it looks like all are green 
-so lets make i keep track of the region which is sorted and let k be the last element that needs sortin 
-then j be the rgion 
-
-*/
 
 void insertionSort2(int arr[], int size) //
 {
@@ -110,16 +80,7 @@ void insertionSort2(int arr[], int size) //
     }
 }
 
-/* insertion sort: USER API
-we have two regions of the array the left side starte with 1 item and it is sorted but the right is not
-the left side continuous to be sorted at all times 
-we look at he first element and since one element is sorted it is already sorted. 
-and we extend from the left region. index i keeps track of unsorted region
-the finale value of sho
-i starte from 1 and goes up tp n-1, n-1 -1 +1 = n-1 checks that there are n-1 items in the unsorted region 
-what keeps track of the sorted region is 0 to i-1 
-end index - start index +1 = the number of element inbetween 
-*/
+
 void insertionSort(int arr[], int size) //
 {
     int i = 1; 
@@ -169,9 +130,6 @@ void SelectionSort(int arr[], int size) //n^2
 }
 
 
-/* ,erge sort
-
-*/
 
 void merge(int arr[], int l, int m, int r) {
     int n1 = m - l + 1;
@@ -234,24 +192,6 @@ void mergeSort(int arr[], int l, int r) {
     }
 }
 
-/*lets say we are solving the problems where in a array we want all the non-zero number in the left side mainting the order 
-and all the zeros in the right side 
-at the beining there is all question mark 
-at some point it t 
-we have three regions the left size has some non-zero numbers the middle region are the element we havent looked at yet
-and the right region are some zeros, lets say j is keeping track of the start of the regions with question marks
-and I keeps track of the region with zeros 
-lets look at the item at j, it can be zero or non zero, the moment we look at the new element regardless the j should increment by 1
-if j is non zero the j should just increment by 1 and no moving is needed, if it zero it should move the i-1 location to maintain the 
-order. and j goes up until the value of i = j is equal, at which point we have looked at all the question marks
-so the inner loop shoudl run until j is smaller than i 
-
-more efficinet application: if we again have three region where the first region is non-zero, the secon region is zereos and the third
-region is question mark. We keep track of the begining of the question mark region using i and the end of the non-zero region with j 
-then we look at the ith item if it is zero then we just increment i, and it it is non-zero then we swap j+1 item with the i item, and then
-increment both j and i
-*/
-
 void moveZeros(int num[], int size)
 {
     int j = 0;
@@ -294,12 +234,6 @@ void moveZeros2(int num[], int size)
         }
     }
 }
-
-
-/* quicksort: look at the array select pivot element them move the object smaller to the left and the element bigger, 
-then choose another pivot do the same and repeat until the array is sorted 
-
-*/
 
 int partitionNew(int arr[], int low, int high) {
     // Randomly choose a pivot
@@ -460,103 +394,259 @@ void quickSortMedianOfMedians(int arr[], int low, int high) {
     }
 }
 
+
+
+
 vector<double> measureSortTime(vector<int>& vector_array, int size, bool worstBubble = false)
 {
     vector<double> time(6);
-    int* arr = new int[size];
-
+    //int* arr = new int[size]; //
+    vector<int> arr(size);
     // 1. Bubble Sort
-    copy(vector_array.begin(), vector_array.end(), arr);
+    copy(vector_array.begin(), vector_array.end(), arr.begin());
     auto bubbleStart = chrono::high_resolution_clock::now();
-    bubbleSort(arr, size);
+    bubbleSort(arr.data(), size);
     auto bubbleEnd = chrono::high_resolution_clock::now();
     //time[0] = chrono::duration_cast<chrono::duration<double>>(bubbleEnd - bubbleStart).count();
     time[0] = chrono::duration_cast<chrono::milliseconds>(bubbleEnd - bubbleStart).count();
 
     // 2. Insertion Sort
-    copy(vector_array.begin(), vector_array.end(), arr);
+    copy(vector_array.begin(), vector_array.end(), arr.begin());
     auto insertionStart = chrono::high_resolution_clock::now();
-    insertionSort(arr, size);
+    insertionSort(arr.data(), size);
     auto insertionEnd = chrono::high_resolution_clock::now();
     //time[1] = chrono::duration_cast<chrono::duration<double>>(insertionEnd - insertionStart).count();
     time[1] = chrono::duration_cast<chrono::milliseconds>(insertionEnd - insertionStart).count();
 
     // 3. Selection Sort
-    copy(vector_array.begin(), vector_array.end(), arr);
+    copy(vector_array.begin(), vector_array.end(), arr.begin());
     auto selectionStart = chrono::high_resolution_clock::now();
-    SelectionSort(arr, size);
+    SelectionSort(arr.data(), size);
     auto selectionEnd = chrono::high_resolution_clock::now();
     //time[2] = chrono::duration_cast<chrono::duration<double>>(selectionEnd - selectionStart).count();
     time[2] = chrono::duration_cast<chrono::milliseconds>(selectionEnd - selectionStart).count();
 
     // 4. Merge Sort
-    copy(vector_array.begin(), vector_array.end(), arr);
+    copy(vector_array.begin(), vector_array.end(), arr.begin());
     auto mergeStart = chrono::high_resolution_clock::now();
-    mergeSort(arr, 0, size - 1);
+    mergeSort(arr.data(), 0, size - 1);
     auto mergeEnd = chrono::high_resolution_clock::now();
     //time[3] = chrono::duration_cast<chrono::duration<double>>(mergeEnd - mergeStart).count();
     time[3] = chrono::duration_cast<chrono::milliseconds>(mergeEnd - mergeStart).count();
 
     // 5. quick with random or worst case 
-    copy(vector_array.begin(), vector_array.end(), arr);
+    copy(vector_array.begin(), vector_array.end(), arr.begin());
     auto quickStart = chrono::high_resolution_clock::now();
     if (worstBubble) {
-        quickSortWorstCase(arr, 0, size);
+        quickSortWorstCase(arr.data(), 0, size);
     } else {
-        quickSortRandomPivot(arr, 0, size);
+        quickSortRandomPivot(arr.data(), 0, size);
     }
     auto quickEnd = chrono::high_resolution_clock::now();
     //time[4] = chrono::duration_cast<chrono::duration<double>>(quickEnd - quickStart).count();
     time[4] = chrono::duration_cast<chrono::milliseconds>(quickEnd - quickStart).count();
 
     // 6. quick sort with MOM
-    copy(vector_array.begin(), vector_array.end(), arr);
+    copy(vector_array.begin(), vector_array.end(), arr.begin());
     auto medianStart = chrono::high_resolution_clock::now();
-    quickSortMedianOfMedians(arr, 0, size);
+    quickSortMedianOfMedians(arr.data(), 0, size);
     auto medianEnd = chrono::high_resolution_clock::now();
     //time[5] = chrono::duration_cast<std::chrono::duration<double>>(medianEnd - medianStart).count();
     time[5] = chrono::duration_cast<chrono::milliseconds>(medianEnd - medianStart).count();
-
-    if (arr != nullptr) {
-    delete[] arr;
-    arr = nullptr;
-    }
     return time;
 }
+
+struct ArrayData {
+    int size;
+    string type;
+    vector<int> elements;
+};
+
+vector<ArrayData> readInputCSV(const string& filename) {
+    vector<ArrayData> arrays; //  read from the file
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        throw runtime_error("Error: Unable to open file " + filename);
+    }
+
+    string line;
+    getline(file, line); // skip the colnames 
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string sizeStr, typeStr, elementsStr;
+
+        // get the array info
+        getline(ss, sizeStr, ',');
+        getline(ss, typeStr, ',');
+        getline(ss, elementsStr, ',');
+
+        int size = stoi(sizeStr); // convert to int
+        vector<int> elements;
+        stringstream elementsStream(elementsStr);
+        int num;
+        while (elementsStream >> num) { // convert to vect
+            elements.push_back(num);
+        }
+
+        arrays.push_back({size, typeStr, elements}); // add to the vector of arrays
+    }
+
+    file.close();
+    return arrays; // Return all parsed arrays
+}
+
+
+void saveToCSVfile(
+    const string& filename,
+    const vector<int>& sizesV,
+    const vector<string>& colNames,
+    const vector<vector<double>>& perfData)
+{
+    filesystem::path filePath = filesystem::current_path() / filename;
+
+    // open in write into the file
+    ofstream file(filePath);
+
+    if (!file.is_open()) {
+        cerr << "Error: Could not open file " << filePath << " for writing." << endl;
+        return;
+    }
+
+    // setting the col names 
+    file << "Size";
+    for (const string& colName : colNames) {
+        file << "," << colName;
+    }
+    file << "\n";
+
+    // Write the data rows
+    for (size_t i = 0; i < sizesV.size(); ++i) {
+        file << sizesV[i]; // size in the first col 
+        for (double perf : perfData[i]) {
+            file << "," << std::fixed << std::setprecision(6) << perf; // Write performance values
+        }
+        file << "\n"; // End the row
+    }
+
+    // Close the file
+    file.close();
+
+    cout << "file saved to: " << filePath << endl;
+}
+
+
 
 
 
 int main()
 {
-    //assesing the perf in alredy sorted arrays aka best case
-    int rows = 40;
-    int cols = 6; 
-    int max_size = 20000;
-    int step = 500; 
+    // assessing the perf in already sorted arrays aka best case
+    int max_size = 150000;
+    int step = 10000;
+    int rows = max_size / step;
+    int cols = 6;
     vector<int> sizesV;
     vector<string> colNames = {"Bubble", "Insertion", "Selection", "Merge", "QuickRandom", "QuickMOM"};
-    vector<vector<double>> sortedArrayPerf(rows, vector<double>(cols, 0.0));
-    //perf for the best time
-    for (int i = 0, size = step; size <= max_size; size += step, i++) {
-
-        sizesV.push_back(size);
-
-        vector<int> sortedArray(size);
-        iota(sortedArray.begin(), sortedArray.end(), 0); // Fill with increasing integers
-
-        // Measure sorting performance for the current array size
-        vector<double> times = measureSortTime(sortedArray, size);
-        // Store the resulting times in sortedArrayPerf
-        sortedArrayPerf[i] = times; // times is the a vector of 6 time for a specific size of array here
-
-    }
     
-    // Plot the sorting performance
-    string savePath = "sorting_performance_best_case_2000_50k.png";
-    plotSortingPerformance(sizesV, colNames, sortedArrayPerf, savePath);
-    cout <<  " end of the loop" << endl;
+    // performance vectors
+    vector<vector<double>> sortedArrayPerf(rows, vector<double>(cols, 0.0));
+    vector<vector<double>> revSortedPerf(rows, vector<double>(cols, 0.0));
+    vector<vector<double>> randomArrayPerf(rows, vector<double>(cols, 0.0));
 
-    //vector<vector<double>> sortedArrayPerf(rows, vector<double>(cols, 0.0));
+    vector<int> sortedArray(max_size);
+    random_device rd;  // Random seed
+    mt19937 g(rd());   // Random number generator
+
+    // if the user does not put a valid file name, it will create array sizes and test the sorting runtime
+    string inputFilename;
+    cout << "Enter an input filename (CSV format with arrays) or press Enter to test auto-generated arrays of diffrent sizes to test: ";
+    getline(cin, inputFilename);
+
+    if (!inputFilename.empty() && filesystem::exists(inputFilename)) {
+        // ff a file is provided, read arrays and measure their performance
+        try {
+            vector<ArrayData> arrays = readInputCSV(inputFilename);
+
+            vector<int> fileSizes;
+            vector<vector<double>> filePerfData(arrays.size(), vector<double>(cols, 0.0));
+
+            for (size_t i = 0; i < arrays.size(); ++i) {
+                fileSizes.push_back(arrays[i].size);
+                filePerfData[i] = measureSortTime(arrays[i].elements, arrays[i].size);
+            }
+
+            // Save results to a CSV
+            saveToCSVfile("InputFilePerformance.csv", fileSizes, colNames, filePerfData);
+            cout << "Performance results for input file saved to InputFilePerformance.csv" << endl;
+
+            // Plot the performance results
+            plotSortingPerformance(fileSizes, colNames, filePerfData, "input_file_performance.png", "Performance from Input File");
+            cout << "Performance plot for input file saved to input_file_performance.png" << endl;
+        } catch (const std::exception& e) {
+            cerr << "Error processing input file: " << e.what() << endl;
+            return 1;
+        }
+    } else if (!inputFilename.empty()) {
+        cerr << "Error: File " << inputFilename << " does not exist." << endl;
+        return 1;
+    } else {
+        for (int i = 0, size = step; size <= max_size; size += step, i++) {
+            sizesV.push_back(size);
+
+            sortedArray.resize(size);
+            iota(sortedArray.begin(), sortedArray.end(), 0); // Fill with increasing integers
+
+            // Measure sorting performance for the current array size
+            vector<double> times = measureSortTime(sortedArray, size);
+            // Store the resulting times in sortedArrayPerf
+            sortedArrayPerf[i] = times; // times is the a vector of 6 time for a specific size of array here
+
+            // Create a reversed array
+            vector<int> reversedArray = sortedArray;
+            reverse(reversedArray.begin(), reversedArray.end()); // reverse the order
+            vector<double> reversedTimes = measureSortTime(reversedArray, size, true); // worst case of the bubble sort choosing the min as pivot
+            // Store the resulting times in the rev sorted perf
+            revSortedPerf[i] = reversedTimes; // Store reversed array times
+
+            // avg case perf of three runs
+            vector<double> avgTimes(cols, 0.0); // Initialize averages to 0
+            for (int run = 0; run < 3; ++run) {
+                vector<int> randomArray = reversedArray;
+                shuffle(randomArray.begin(), randomArray.end(), g); // create a copy of shuffled
+                vector<double> randomTimes = measureSortTime(randomArray, size); // random time of the six algorithms
+                for (int j = 0; j < cols; ++j) {
+                    avgTimes[j] += randomTimes[j]; // random time added together
+                }
+            }
+            for (int j = 0; j < cols; ++j) {
+                avgTimes[j] /= 3.0; // the accumulated average
+            }
+            randomArrayPerf[i] = avgTimes; // store the average times for a given size
+        } // for loop
+
+            // Plot the sorting performance
+        string savePath = "sorting_performance_best_case.png";
+        plotSortingPerformance(sizesV, colNames, sortedArrayPerf, savePath, "Sorting Performance on Sorted Array");
+
+        // plot the rev sorted perf
+        string revSavePath = "sorting_performance_rev_case.png";
+        plotSortingPerformance(sizesV, colNames, revSortedPerf, revSavePath, "Sorting Performance on Reversed Array");
+
+        // plot the Avg case
+        string AvgSavePath = "sorting_performance_Avg_case.png";
+        plotSortingPerformance(sizesV, colNames, randomArrayPerf, AvgSavePath, "Sorting Performance on Random Array");
+
+
+        saveToCSVfile("SortedArrayPerformance.csv", sizesV, colNames, sortedArrayPerf);
+        saveToCSVfile("RevArrayPerformance.csv", sizesV, colNames, revSortedPerf);
+        saveToCSVfile("AvgArrayPerformance.csv", sizesV, colNames, randomArrayPerf);
+        
+        // each algorithm separately
+        plotComparisonPerformance(sizesV, colNames, sortedArrayPerf, revSortedPerf, randomArrayPerf);
+
+    } // else
 
     return 0;
 }
